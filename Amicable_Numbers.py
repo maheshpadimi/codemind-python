cc68d05bc4dde525e25011518a090ed643d037fe
Mahesh padimi <82667979+maheshpadimi@users.noreply.github.com>
2022-10-27 14:48:14 +0530

    a = int(input()) b = int(input()) count=0 flag=0 for i in range(1,a):     if a%i==0:         count+=i for j in range(1,b):     if b%j==0:         flag+=j if count==b and flag==a:     print('Amicable') else:     print('Not Amicable')

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,14 +1,14 @@
 a = int(input())
 b = int(input())
-s = 0
-t = 0
+count=0
+flag=0
 for i in range(1,a):
     if a%i==0:
-        s+=i
+        count+=i
 for j in range(1,b):
     if b%j==0:
-        t+=j
-if s==b and t==a:
-    print("Amicable")
+        flag+=j
+if count==b and flag==a:
+    print('Amicable')
 else:
-    print("Not Amicable")
+    print('Not Amicable')
