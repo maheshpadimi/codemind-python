1a99dfa47f4639cfb8d7cc6429510e6bd4f7a90a
Mahesh padimi <82667979+maheshpadimi@users.noreply.github.com>
2022-10-27 15:08:58 +0530

    n = input() a = set(n) if len(n)==len(a):     print("Unique Number") else:     print("Not Unique Number")

--- Unique_number.py
+++ Unique_number.py
@@ -1,17 +1,6 @@
-n = int(input())
-l = []
-flag =0
-while n!=0:
-    r = n%10
-    l.append(r)
-    n=n//10
-for i in l:
-    if l.count(i)>1:
-        flag=1
-        break
-    else:
-        flag=0
-if flag==1:
-    print("Not Unique Number")
+n = input()
+a = set(n)
+if len(n)==len(a):
+    print("Unique Number")
 else:
-    print('Unique Number')
+    print("Not Unique Number")
